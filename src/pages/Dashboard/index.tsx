b2ada6847bf887dcbb8b1c85cb791dade2d33e98
Daniel Versiane <daniel.versiane@gmail.com>
2020-12-01 21:39:58 -0300

    add: gerenciamento de rotas com autenticação.

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <span>Dashboard</span>
+    </Container>
+  )
+}
+
+export default Dashboard
