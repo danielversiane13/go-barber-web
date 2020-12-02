f8e64863f9dfd1a2581fb00921678d574ca6464b
Daniel Versiane <daniel.versiane@gmail.com>
2020-12-01 22:04:23 -0300

    add: comunicação com api page SignUp.
    add: temporário alterações na dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 
+import Button from '../../components/Button'
+import { useAuth } from '../../hooks/auth'
+
 import { Container } from './styles'
 
 const Dashboard: React.FC = () => {
+  const { signOut } = useAuth()
   return (
     <Container>
-      <span>Dashboard</span>
+      <div>
+        <h1>Dashboard</h1>
+        <h2>Você está logado !</h2>
+        <Button onClick={signOut}>Deslogar</Button>
+      </div>
     </Container>
   )
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,18 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 100vw;
+  height: 80vh;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 500px;
+  }
+`
